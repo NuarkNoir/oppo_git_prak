cf4e2a5c1bd678ef9aa9db886008d073d7bd7467
Andrew nuark G <mrcluster@ya.ru>
2020-11-21 01:54:53 +0700

    Fixed exponentiation function for negative powers

--- main.cpp
+++ main.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 
 
-double my_pow(double num, unsigned int pow);
+double my_pow(double num, int pow);
 
 int main() {
     return 0;
 }
 
-double my_pow(double num, unsigned int pow) {
-    double out = num;
+double my_pow(double num, int pow) {
+    double out = 1;
+    bool neg = pow < 0;
 
-    for (int i = 1; i < pow; i++) {
-        out *= num;
+    if (neg) {
+        pow *= -1;
+        for (int i = 0; i < pow; i++) {
+            out /= num;
+        }
+    }
+    else {
+        for (int i = 0; i < pow; i++) {
+            out *= num;
+        }
     }
 
     return out;
